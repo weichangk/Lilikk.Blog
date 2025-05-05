48c289ab2a2cd0ec1f12e2aa48f02c5b0a11566d
weichangk <18276743761@163.com>
2025-05-05 19:29:23 +0800

    first commit

---
+++ src/backend/Easy.Admin.Application/Api/ApiService.cs
@@ -0,0 +1,32 @@
+﻿using Furion.HttpRemote;
+
+namespace Easy.Admin.Application;
+
+/// <summary>
+/// 第三方API调用
+/// </summary>
+public class ApiService : IScoped
+{
+    private const string BaseUrl = "https://api.wer.plus";
+
+    /// <summary>
+    /// 检查 文本内容是否包含违禁词
+    /// </summary>
+    /// <param name="content">内容</param>
+    /// <returns></returns>
+    public async Task<bool> Keywords([FromQuery] string content)
+    {
+        try
+        {
+            var httpRemoteService = App.GetRequiredService<IHttpRemoteService>();
+            var dto = await httpRemoteService.GetAsAsync<KeywordsResponseDto>($"{BaseUrl}/api/min");
+            //var dto = await $"{BaseUrl}/api/min".SetQueries(new { t = content }).GetAsAsync<KeywordsResponseDto>();
+            return dto.Success && dto.Num > 0;
+
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
